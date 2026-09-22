48760c0b5a524c44e4b2510f8dca8af3cbef8c91
masonFei <994085848@qq.com>
2026-09-22 22:30:06 +0800

    20_对话模型之ChatClient自动注入

--- saaAtguiguV1/SAA-03ChatModelChatClient/src/main/java/com/fxy/config/SaaLLMConfig.java
+++ saaAtguiguV1/SAA-03ChatModelChatClient/src/main/java/com/fxy/config/SaaLLMConfig.java
@@ -5,6 +5,8 @@
 package com.fxy.config;
 
 import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +27,11 @@ public class SaaLLMConfig {
     @Bean
     public DashScopeApi dashScopeApi() {
         return DashScopeApi.builder().apiKey(apiKey).build();
+    }
+
+    @Bean
+    public ChatClient chatClient(ChatModel dashscopeChatModel) {
+        return ChatClient.builder(dashscopeChatModel).build();
     }
 
 }
---
+++ saaAtguiguV1/SAA-03ChatModelChatClient/src/main/java/com/fxy/controller/ChatClientControllerV2.java
@@ -0,0 +1,54 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package com.fxy.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ChatClientControllerV2
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-09-22 22:19
+ */
+@RestController
+public class ChatClientControllerV2 {
+
+    @Resource
+    private ChatModel chatModel;
+
+    @Resource
+    private ChatClient dashscopeChatClientV2;
+
+    /**
+     * 通用调用
+     *
+     * @param msg
+     * @return
+     */
+    @GetMapping(value = "/chatclientV2/dochat")
+    public String doChat(@RequestParam(name = "msg", defaultValue = "你是谁") String msg) {
+        String result = chatModel.call(msg);
+        return result;
+    }
+
+    /**
+     * 调用chatClient
+     *
+     * @param msg
+     * @return
+     */
+    @GetMapping(value = "/chatclientV2/chatClient")
+    public String doChatWithChatClient(@RequestParam(name = "msg", defaultValue = "2+9等于几") String msg) {
+        return dashscopeChatClientV2.prompt().user(msg).call().content();
+    }
+
+
+}
