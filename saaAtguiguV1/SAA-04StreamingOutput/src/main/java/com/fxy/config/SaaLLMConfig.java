543d737978e13c8fb484473162ec31686a1fb43b
masonFei <994085848@qq.com>
2026-09-25 21:52:16 +0800

    26_SSE之前后端编码联调

--- saaAtguiguV1/SAA-04StreamingOutput/src/main/java/com/fxy/config/SaaLLMConfig.java
+++ saaAtguiguV1/SAA-04StreamingOutput/src/main/java/com/fxy/config/SaaLLMConfig.java
@@ -7,7 +7,10 @@ package com.fxy.config;
 import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -48,5 +51,20 @@ public class SaaLLMConfig {
                 .defaultOptions(DashScopeChatOptions.builder().withModel(QWEN_MODEL).build())
                 .build();
     }
+
+    @Bean(name = "deepseekChatClient")
+    public ChatClient deepseekChatClient(@Qualifier("deepseek") ChatModel deepseek) {
+        return ChatClient.builder(deepseek)
+                .defaultOptions(ChatOptions.builder().model(DEEPSEEK_MODEL).build())
+                .build();
+    }
+
+    @Bean(name = "qwenChatClient")
+    public ChatClient qwenChatClient(@Qualifier("qwen") ChatModel qwen) {
+        return ChatClient.builder(qwen)
+                .defaultOptions(ChatOptions.builder().model(QWEN_MODEL).build())
+                .build();
+    }
+
 
 }
--- saaAtguiguV1/SAA-04StreamingOutput/src/main/java/com/fxy/controller/StreamOutputController.java
+++ saaAtguiguV1/SAA-04StreamingOutput/src/main/java/com/fxy/controller/StreamOutputController.java
@@ -5,7 +5,9 @@
 package com.fxy.controller;
 
 import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,13 @@ public class StreamOutputController {
 
     @Resource(name = "qwen")
     private ChatModel qwenChatModel;
+
+    @Resource(name = "deepseekChatClient")
+    private ChatClient deepseekChatClient;
+
+    @Resource(name = "qwenChatClient")
+    private ChatClient qwenChatClient;
+
 
     /**
      * 流式返回调用
@@ -49,6 +58,29 @@ public class StreamOutputController {
     public Flux<String> chatflux2(@RequestParam(name = "msg", defaultValue = "你是谁") String msg) {
         Flux<String> result = qwenChatModel.stream(msg);
         return result;
+    }
+
+
+    /**
+     * 流式返回调用
+     *
+     * @param msg
+     * @return
+     */
+    @GetMapping(value = "/stream/chatflux3")
+    public Flux<String> chatflux3(@RequestParam(name = "msg", defaultValue = "你是谁") String msg) {
+        return deepseekChatClient.prompt(msg).stream().content();
+    }
+
+    /**
+     * 流式返回调用
+     *
+     * @param msg
+     * @return
+     */
+    @GetMapping(value = "/stream/chatflux4", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> chatflux4(@RequestParam(name = "msg", defaultValue = "你是谁") String msg) {
+        return qwenChatClient.prompt(msg).stream().content();
     }
 
 
